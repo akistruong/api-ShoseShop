72d96ae7685356749e990c45c725095c38f2e6fd
caokiet <truongkiet.hn288@gmail.com>
2022-02-12 23:29:24 +0700

    init User Object

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,8 +1,10 @@
 const ProductRoute = require("./Product.route");
 const CategoryRoute = require("./Category.routes");
+const UserRoute = require("./User.route");
 const routes = (app) => {
   app.use("/api", ProductRoute);
   app.use("/api", CategoryRoute);
+  app.use("/api", UserRoute);
 };
 
 module.exports = routes;
