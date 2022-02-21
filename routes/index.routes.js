2842414f245a30513c36f54964047770e77bf8ab
caokiet <truongkiet.hn288@gmail.com>
2022-02-21 22:17:12 +0700

    upload index.routes.js

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,11 +1,14 @@
 const ProductRoute = require("./Product.route");
 const CategoryRoute = require("./Category.routes");
-
+const AuthRoute = require("./Auth.route");
 const UploadRoute = require("./Upload.route");
+const UserRoute = require("./User.route");
 const routes = (app) => {
   app.use("/api", ProductRoute);
   app.use("/api", CategoryRoute);
   app.use("/api", UploadRoute);
+  app.use("/api", AuthRoute);
+  app.use("/api", UserRoute);
 };
 
 module.exports = routes;
